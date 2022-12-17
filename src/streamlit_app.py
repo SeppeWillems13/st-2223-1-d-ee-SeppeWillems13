3f17672668f42fd504c06ffc85d169b2c9e2412a
Seppe Willems <seppe.willems2@student.howest.be>
2022-12-17 13:35:03 +0100

    Fix name bug

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -12,6 +12,8 @@ score = {'wins': 0, 'losses': 0, 'ties': 0}
 
 while True:
     # Get the player's name
+    #fix random bug
+    st.experimental_reset()
     player_name = st.text_input('Enter your name:')
 
     # Get the number of rounds to play
