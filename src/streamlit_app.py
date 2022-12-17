87b70d25f30b97fe25910d2784584caf0f900455
Seppe Willems <seppe.willems2@student.howest.be>
2022-12-17 13:18:47 +0100

    Write simple streamlit rock paper scissor

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,67 +1,31 @@
-import cv2
 import streamlit as st
+import random
 
-# Load the hand detection cascade classifier
-hand_cascade = cv2.CascadeClassifier('hand.xml')
+# Create a list of options
+options = ['rock', 'paper', 'scissors']
 
-# Create a Streamlit app
-@st.cache
-def main():
-  st.title("Seppe project")
+# Start the game
+st.title('Rock, Paper, Scissors')
+st.write('Choose your option:')
 
-  # Set up the camera
-  cap = cv2.VideoCapture(0)
+# Get the player's choice
+player_choice = st.selectbox('Your turn:', options)
 
-  # Set up the game logic
-  def play_game(player_choice, computer_choice):
-    if player_choice == computer_choice:
-      return "Tie"
-    elif player_choice == "rock" and computer_choice == "scissors":
-      return "Player wins"
-    elif player_choice == "paper" and computer_choice == "rock":
-      return "Player wins"
-    elif player_choice == "scissors" and computer_choice == "paper":
-      return "Player wins"
-    else:
-      return "Computer wins"
+# Get the computer's choice
+computer_choice = random.choice(options)
 
-  # Set up the hand gestures
-  gestures = ["rock", "paper", "scissors"]
+# Compare the choices and determine the winner
+if player_choice == computer_choice:
+    result = 'It\'s a tie!'
+elif player_choice == 'rock' and computer_choice == 'scissors':
+    result = 'You win!'
+elif player_choice == 'paper' and computer_choice == 'rock':
+    result = 'You win!'
+elif player_choice == 'scissors' and computer_choice == 'paper':
+    result = 'You win!'
+else:
+    result = 'You lose!'
 
-  # Create buttons for the player to select their hand gesture
-  player_choice = st.sidebar.radio("Select your hand gesture:", gestures)
-
-  # Capture and display the video feed
-  while True:
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect hands in the video feed
-    hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
-
-    # Iterate through the detected hands
-    for (x,y,w,h) in hands:
-      # Draw a rectangle around the detected hand
-      cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-      roi_gray = gray[y:y+h, x:x+w]
-      roi_color = frame[y:y+h, x:x+w]
-
-      # Label the detected hand as "rock," "paper," or "scissors"
-      if w > 100:
-        cv2.putText(frame, "rock", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3)
-        computer_choice = "rock"
-      elif w > 50:
-        cv2.putText(frame, "paper", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3)
-        computer_choice = "paper"
-      else:
-        cv2.putText(frame, "scissors", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3)
-        computer_choice = "scissors"
-
-    # Display the video feed
-    st.image(frame)
-
-    # Play the game and display the results
-    if st.sidebar.button("Play"):
-      result = play_game(player_choice, computer_choice)
-      st.write("Result: ", result)
+# Display the results
+st.write(f'You chose {player_choice} and the computer chose {computer_choice}')
+st.write(result)
