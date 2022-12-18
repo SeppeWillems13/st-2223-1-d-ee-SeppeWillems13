a7402b3e3c898882f566f2ff440aa553d20c7ae3
Seppe Willems <seppe.willems2@student.howest.be>
2022-12-18 14:48:18 +0100

    Add basic game stats

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # import the necessary modules
 import random
 
+# ask the player for their username
+username = input('Enter your username: ')
 # define the possible choices for the game
 choices = ['rock', 'paper', 'scissors']
 
@@ -11,10 +13,19 @@ win_conditions = [
     ('scissors', 'paper')
 ]
 
+# initialize game statistics
+wins = 0
+losses = 0
+ties = 0
+
 # main game loop
-while True:
+while username:
     # get the player's choice
     player_choice = input('Choose rock, paper, or scissors: ')
+
+    # check if the player wants to stop the game
+    if player_choice == 'stop':
+        break
 
     # validate the player's choice
     if player_choice not in choices:
@@ -27,7 +38,13 @@ while True:
     # compare the choices and determine the winner
     if (player_choice, computer_choice) in win_conditions:
         print(f'You win! {player_choice} beats {computer_choice}.')
+        wins += 1
     elif (computer_choice, player_choice) in win_conditions:
         print(f'You lose! {computer_choice} beats {player_choice}.')
+        losses += 1
     else:
         print(f'It\'s a tie! Both players chose {player_choice}.')
+        ties += 1
+
+    # display game statistics
+    print(f'Wins: {wins} Losses: {losses} Ties: {ties}')
