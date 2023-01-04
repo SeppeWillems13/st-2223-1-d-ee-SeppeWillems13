f864d4050024732bddada54dbc9d5c45971ff7f6
seppe <seppe.willems2@student.howest.be>
2023-01-04 12:36:15 +0100

    Add simple web server django

--- src/hand_recognition/ImageProcessor.py
+++ src/hand_recognition/ImageProcessor.py
@@ -23,26 +23,28 @@ class ImageProcessor:
         img_crop = cv2.GaussianBlur(img_crop, (5, 5), 0)
 
         # Calculate the edge map of the image
-        img_crop = cv2.Canny(img_crop, 100, 200)
+        img_crop = cv2.Canny(img_crop, 100, 200, L2gradient=True)
 
         # Calculate the HOG descriptor of the image
-        img_hog = cv2.HOGDescriptor()
-        img_crop = img_hog.compute(img_crop)
+        win_size = (32, 32)
+        block_size = (16, 16)
+        block_stride = (8, 8)
+        cell_size = (8, 8)
+        num_bins = 9
+        hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, num_bins)
+        img_hog = hog.compute(img_crop)
 
         # Normalize the pixel values to the range [0, 1]
-        img_crop = np.interp(img_crop, (img_crop.min(), img_crop.max()), (0, 1))
+        img_hog = np.interp(img_hog, (img_hog.min(), img_hog.max()), (0, 1))
 
         # Convert the image data to depth 8 bits per pixel
-        img_crop = cv2.normalize(img_crop, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        img_hog = cv2.normalize(img_hog, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
         # Apply median filtering to the image to remove noise
-        img_crop = cv2.medianBlur(img_crop, ksize=5)
-
-        # Convert the image to grayscale
-        img_crop = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+        img_hog = cv2.medianBlur(img_hog, ksize=5)
 
         # Resize the image to 300x300
-        img_crop = cv2.resize(img_crop, (self.img_size, self.img_size))
+        img_crop = cv2.resize(img_hog, (self.img_size, self.img_size))
 
         # Add a third dimension to the resized image to match the shape of the white image
         img_resize = np.expand_dims(img_crop, axis=2)
@@ -51,5 +53,4 @@ class ImageProcessor:
         img_white = np.ones((self.img_size, self.img_size, 3), np.uint8) * 255
 
         return img_white, img_resize
-
 
