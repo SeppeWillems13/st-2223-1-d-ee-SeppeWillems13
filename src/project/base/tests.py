5223b799b79c7e4dc6787ffa99896c4574020779
seppe <seppe.willems2@student.howest.be>
2023-01-21 00:10:50 +0100

    Add basic tests.py

--- src/project/base/tests.py
+++ src/project/base/tests.py
@@ -1,1 +1,84 @@
-# Create your tests here.
+from django.test import TestCase
+
+from .models import Room, User, Player
+
+
+class RoomModelTests(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="testuser", password="testpassword")
+        self.room1 = Room.objects.create(host=user, name="Test Room 1", code="testcode1", online=True)
+        self.room2 = Room.objects.create(host=user, name="Test Room 2", code="testcode2", online=False)
+
+    def test_create_room(self):
+        user = User.objects.create(username="testuser2", password="testpassword")
+        new_room = Room.objects.create(host=user, name="Test Room 3", code="testcode3", online=True)
+
+        self.assertEqual(new_room.name, "Test Room 3")
+        self.assertEqual(new_room.code, "testcode3")
+        self.assertEqual(new_room.online, True)
+
+    def test_update_room(self):
+        self.room1.name = "Updated Test Room 1"
+        self.room1.code = "updatedtestcode1"
+        self.room1.online = False
+        self.room1.save()
+
+        self.assertEqual(self.room1.name, "Updated Test Room 1")
+        self.assertEqual(self.room1.code, "updatedtestcode1")
+        self.assertEqual(self.room1.online, False)
+
+    def test_delete_room(self):
+        self.room1.delete()
+        self.assertEqual(Room.objects.count(), 1)
+
+    def test_join_room(self):
+        test_user = User.objects.create(username='testuser3')
+        Player.objects.filter(user=test_user).delete()
+        test_player = Player.objects.create(user=test_user)
+
+        self.room1.players.add(test_player)
+        self.assertIn(test_player, self.room1.players.all())
+
+    def test_leave_room(self):
+        test_user = User.objects.create_user(username="testuser4", password="testpassword")
+        Player.objects.filter(user=test_user).delete()
+        test_player = Player.objects.create(user=test_user)
+
+        self.room1.players.add(test_player)
+        self.assertIn(test_player, self.room1.players.all())
+
+        self.room1.players.remove(test_player)
+        self.assertNotIn(test_player, self.room1.players.all())
+
+# class GameModelTests(TestCase):
+#     def setUp(self):
+#
+#     def test_create_game(self):
+#
+#     def test_update_game(self):
+#
+#
+#     def test_delete_game(self):
+#
+#
+#     def test_play_round(self):
+#
+#
+#     def test_check_if_game_is_done(self):
+#
+#
+# class UserModelTests(TestCase):
+#     def setUp(self):
+#
+#     def test_create_user(self):
+#
+#     def test_update_user(self):
+#
+#     def test_delete_user(self):
+#
+#     def test_authenticate_user(self):
+#
+#     def test_login_user(self):
+#
+#     def test_logout_user(self):
+
--- src/project/project/settings.py
+++ src/project/project/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
